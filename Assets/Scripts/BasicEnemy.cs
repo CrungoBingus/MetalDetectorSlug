cb63383b1163606d3d24a6483e01d40c46dcd926
Sam <samuel.canonaco@ontariotechu.net>
2020-10-17 11:02:35 -0400

    Little enemy
    I made a little enemy that just stand there and shoots, I don't know if nav meshes work in 2d

---
+++ Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float timeSinceShoot=0f;
+    Vector3 shootPos;
+    Quaternion shootRot;
+
+    [SerializeField] private GameObject bullet;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceShoot += Time.deltaTime;
+        if(timeSinceShoot>=1.5f)
+        {
+            timeSinceShoot = 0f;
+            shootPos= transform.position + new Vector3(-0.5f, 0f, 0f);
+            shootRot = Quaternion.Euler(0f, 0f, Random.Range(180f, 130f));
+            Instantiate(bullet, shootPos, shootRot);
+
+        }
+    }
+}
