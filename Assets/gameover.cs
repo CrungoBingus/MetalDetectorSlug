c8238ef4a9704ccf340c50ca8632fca952d6386c
Robson Basha <ryuhoshi101@gmail.com>
2020-10-18 14:44:39 -0400

    Added Le Awesome Game Over

---
+++ Assets/gameover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Gameover());
+    }
+
+    IEnumerator Gameover()
+    {
+        yield return new WaitForSeconds(6);
+        SceneManager.LoadScene("Menu");
+    }
+}
